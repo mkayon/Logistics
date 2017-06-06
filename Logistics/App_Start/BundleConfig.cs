aa58f3de31cb91541eaf5f327cea1444a6a2979f
mkayon <mkayon@yandex.ua>
2017-06-06 11:52:36 +0300

    add arrow up-down script

--- Logistics/App_Start/BundleConfig.cs
+++ Logistics/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace Logistics
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                        "~/Scripts/arrow-panel.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ Logistics/Scripts/arrow-panel.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    $(document).on('click', '.clickable', function (e) {
+        var $this = $(this);
+
+        if ($this.find('i').hasClass('glyphicon-chevron-up')) {
+            $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        }
+        else if ($this.find('i').hasClass('glyphicon-chevron-down')) {
+            $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        }
+    })
+})();
